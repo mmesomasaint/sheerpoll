e5299021889c1d39a24c774d8de4e205ee889aa3
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-17 22:56:13 +0100

    Fix: Format with prettier

--- app/v/dashboard/page.tsx
+++ app/v/dashboard/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRouter } from "next/navigation"
+import { useRouter } from 'next/navigation'
 
 export default function Dashboard() {
   return (
@@ -17,11 +17,11 @@ function PositionAccordion() {
   const router = useRouter()
 
   return (
-    <div className="bg-primary/30 first:border-t first:border-t-primary border-b border-b-primary px-14 py-7">
-      <div className="flex flex-col items-start gap-2">
-        <p className="text-xs font-medium">QER313EHNT_HHGRI58Y3</p>
-        <div className="flex justify-between items-center gap-20">
-          <h3 className="text-4xl font-semibold text-">Vice President</h3>
+    <div className='bg-primary/30 first:border-t first:border-t-primary border-b border-b-primary px-14 py-7'>
+      <div className='flex flex-col items-start gap-2'>
+        <p className='text-xs font-medium'>QER313EHNT_HHGRI58Y3</p>
+        <div className='flex justify-between items-center gap-20'>
+          <h3 className='text-4xl font-semibold text-'>Vice President</h3>
           <button
             type='button'
             onClick={() => router.push('/v/dashboard/vote/pos_id')}
