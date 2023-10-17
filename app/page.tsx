1b2f7d5531dc18b1bd9b2b1a0be207b1dbcf517b
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-17 19:28:47 +0100

    Fix: Correct the auth addr

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
           <h1 className='text-4xl font-bold'>Sheerpoll</h1>
         </div>
         <div className='flex justify-end items-center gap-10'>
-          <Link href='/auth/login' className='text-lg font-bold'>
+          <Link href='/v/auth/login' className='text-lg font-bold'>
             Login
           </Link>
-          <Link href='/auth/signup' className='text-lg font-bold'>
+          <Link href='/v/auth/signup' className='text-lg font-bold'>
             Signup
           </Link>
         </div>
@@ -22,10 +22,10 @@ export default function Home() {
           A pure and transparent election conducting platform
         </p>
         <div className='flex justify-center items-center gap-10'>
-          <Link href='/auth/login' className='text-lg font-bold'>
+          <Link href='/v/auth/login' className='text-lg font-bold'>
             Login
           </Link>
-          <Link href='/auth/signup' className='text-lg font-bold'>
+          <Link href='/v/auth/signup' className='text-lg font-bold'>
             Signup
           </Link>
         </div>
