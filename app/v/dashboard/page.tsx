71e88188216f2e0cfd16974551a7242e1de928cd
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-17 22:30:19 +0100

    Feat: Split dashboard page and layout

--- app/v/dashboard/page.tsx
+++ app/v/dashboard/page.tsx
@@ -1,22 +1,7 @@
-import Link from 'next/link'
-
 export default function Dashboard() {
   return (
-    <div className='flex flex-col min-h-screen gap-0'>
-      <div className='py-16'>
-        <h1 className='text-6xl font-bold'>Hello, (name)</h1>
-      </div>
-      <div className='grow flex flex-col h-full'>
-        <div className='flex justify-start items-center gap-20'>
-          <Link href='/v/dashboard/'>
-            <p className='text-base font-semibold'>Timeline</p>
-          </Link>
-          <Link href='/v/dashboard/election'>
-            <p className='text-base font-semibold'>Election</p>
-          </Link>
-        </div>
-        <div className='grow h-full'>Loading.. or Content goes here..</div>
-      </div>
+    <div className='flex flex-col gap-0'>
+      Loading.. or Content goes here..
     </div>
   )
 }
