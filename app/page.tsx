a76a91eef7c09535a09f52d258fd101610caf762
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-17 19:26:56 +0100

    Feat: Improve on the styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between'>
-      <div className='flex justify-between items-center gap-10 p-10 my-20'>
+      <div className='flex justify-between items-center gap-10 px-10 py-5 w-full'>
         <div className='flex justify-start items-center gap-10'>
           <h1 className='text-4xl font-bold'>Sheerpoll</h1>
         </div>
