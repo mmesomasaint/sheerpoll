d9a3977cb4ca36c4d888f98a24ccaae0a7e4331f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-17 22:55:59 +0100

    Feat: Create and test the position accordion

--- app/v/dashboard/page.tsx
+++ app/v/dashboard/page.tsx
@@ -1,7 +1,36 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+
 export default function Dashboard() {
   return (
     <div className='flex flex-col gap-0'>
-      Loading.. or Content goes here..
+      <PositionAccordion />
+      <PositionAccordion />
+      <PositionAccordion />
+      <PositionAccordion />
+    </div>
+  )
+}
+
+function PositionAccordion() {
+  const router = useRouter()
+
+  return (
+    <div className="bg-primary/30 first:border-t first:border-t-primary border-b border-b-primary px-14 py-7">
+      <div className="flex flex-col items-start gap-2">
+        <p className="text-xs font-medium">QER313EHNT_HHGRI58Y3</p>
+        <div className="flex justify-between items-center gap-20">
+          <h3 className="text-4xl font-semibold text-">Vice President</h3>
+          <button
+            type='button'
+            onClick={() => router.push('/v/dashboard/vote/pos_id')}
+            className='px-7 py-3 text-white bg-primary rounded-md shadow-sm'
+          >
+            Vote
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
