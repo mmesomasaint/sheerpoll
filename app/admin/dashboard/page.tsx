522910ad7f955948722a1218ba768daf530820dc
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-18 19:58:42 +0100

    Fix: Format with prettier

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,17 +1,17 @@
 'use client'
 
-import {useState} from 'react'
+import { useState } from 'react'
 import { useAuth } from '../auth/auth'
 import Link from 'next/link'
 
 export default function Dashboard() {
-  const {admin} = useAuth()
+  const { admin } = useAuth()
 
   return (
     <div className='flex flex-col items-start min-h-screen px-20'>
       <div className='flex flex-col items-start gap-10'>
-      <h6 className='text-sm font-medium'>Welcome, {admin?.auth}</h6>
-      <h2 className='text-6xl font-semibold'>Positions</h2>
+        <h6 className='text-sm font-medium'>Welcome, {admin?.auth}</h6>
+        <h2 className='text-6xl font-semibold'>Positions</h2>
       </div>
       <div className='flex flex-col'>
         <div className='flex justify-start items-center gap-10'>
@@ -22,9 +22,7 @@ export default function Dashboard() {
             <p className='text-base font-semibold'>Concluded</p>
           </Link>
         </div>
-        <div className='grow flex px-20 flex-col'>
-          Nothing to show here...
-        </div>
+        <div className='grow flex px-20 flex-col'>Nothing to show here...</div>
       </div>
     </div>
   )
