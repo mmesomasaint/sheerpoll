a202967e9c0c3581205c68c8adfae351603a15ae
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-18 08:54:39 +0100

    Feat: Bring the header home

--- app/v/dashboard/page.tsx
+++ app/v/dashboard/page.tsx
@@ -1,15 +1,36 @@
 'use client'
 
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { useAuth } from '../auth/auth'
 
 export default function Timeline() {
+  const {voter} = useAuth()
+
   return (
+      <div className='flex flex-col min-h-screen gap-0'>
+        <div className='py-16'>
+          <h1 className='text-6xl font-bold'>Hello, {voter?.displayName}</h1>
+        </div>
+        <div className='grow flex flex-col h-full'>
+          <div className='flex justify-start items-center gap-20'>
+            <Link href='/v/dashboard/'>
+              <p className='text-base font-semibold'>Timeline</p>
+            </Link>
+            <Link href='/v/dashboard/election'>
+              <p className='text-base font-semibold'>Election</p>
+            </Link>
+          </div>
+          <div className='grow h-full'>
     <div className='flex flex-col gap-0'>
       <PositionAccordion />
       <PositionAccordion />
       <PositionAccordion />
       <PositionAccordion />
     </div>
+    </div>
+        </div>
+      </div>
   )
 }
 
