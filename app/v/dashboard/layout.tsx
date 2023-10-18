63772733a9ade641069d25720e8035e411832484
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-18 07:25:09 +0100

    Feat: Test run the auth context

--- app/v/dashboard/layout.tsx
+++ app/v/dashboard/layout.tsx
@@ -1,22 +1,25 @@
 import Link from 'next/link'
+import { AuthProvider } from '../auth/auth'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex flex-col min-h-screen gap-0'>
-      <div className='py-16'>
-        <h1 className='text-6xl font-bold'>Hello, (name)</h1>
-      </div>
-      <div className='grow flex flex-col h-full'>
-        <div className='flex justify-start items-center gap-20'>
-          <Link href='/v/dashboard/'>
-            <p className='text-base font-semibold'>Timeline</p>
-          </Link>
-          <Link href='/v/dashboard/election'>
-            <p className='text-base font-semibold'>Election</p>
-          </Link>
+    <AuthProvider>
+      <div className='flex flex-col min-h-screen gap-0'>
+        <div className='py-16'>
+          <h1 className='text-6xl font-bold'>Hello, (name)</h1>
         </div>
-        <div className='grow h-full'>{children}</div>
+        <div className='grow flex flex-col h-full'>
+          <div className='flex justify-start items-center gap-20'>
+            <Link href='/v/dashboard/'>
+              <p className='text-base font-semibold'>Timeline</p>
+            </Link>
+            <Link href='/v/dashboard/election'>
+              <p className='text-base font-semibold'>Election</p>
+            </Link>
+          </div>
+          <div className='grow h-full'>{children}</div>
+        </div>
       </div>
-    </div>
+    </AuthProvider>
   )
 }
