f43e1dfcb6da9c27dc2ade03f8e93f3afe5083f9
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-18 09:07:00 +0100

    Feat: Restructure election and timeline tabs

--- app/v/dashboard/election/page.tsx
+++ app/v/dashboard/election/page.tsx
@@ -1,31 +1,59 @@
 'use client'
 
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { useAuth } from '../../auth/auth'
 
 export default function Election() {
+  const { voter } = useAuth()
+
   return (
-    <div className='flex flex-col gap-0'>
-      <VotedPositionAccordion />
-      <VotedPositionAccordion />
-      <VotedPositionAccordion />
-      <VotedPositionAccordion />
+    <div className='flex flex-col min-h-screen gap-0'>
+      <div className='flex flex-col items-start py-10 gap-5'>
+        <h1 className='text-sm font-semibold'>Hello, {voter?.displayName}</h1>
+        <h1 className='text-6xl font-semibold'>Election</h1>
+      </div>
+      <div className='grow flex flex-col h-full'>
+        <div className='flex justify-start items-center gap-20'>
+          <Link href='/v/dashboard/'>
+            <p className='text-base font-semibold'>Timeline</p>
+          </Link>
+          <Link href='/v/dashboard/election'>
+            <p className='text-base font-semibold'>Election</p>
+          </Link>
+        </div>
+        <div className='grow h-full'>
+          <div className='flex flex-col gap-0'>
+            <PositionAccordion />
+            <PositionAccordion />
+            <PositionAccordion />
+            <PositionAccordion />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-function VotedPositionAccordion() {
+function PositionAccordion() {
   const router = useRouter()
 
   return (
-    <div className='bg-primary/10 first:border-t first:border-t-primary border-b border-b-primary px-14 py-7'>
+    <div className='bg-primary/30 first:border-t first:border-t-primary border-b border-b-primary px-14 py-7'>
       <div className='flex flex-col items-start gap-2'>
         <p className='text-xs font-medium'>QER313EHNT_HHGRI58Y3</p>
-        <div className='flex justify-start items-center gap-4'>
+        <div className='flex justify-between items-center gap-20'>
           <h3 className='text-4xl font-semibold text-'>Vice President</h3>
-          <h3 className='text-2xl font-semibold text-'>&rarr;</h3>
-          <h3 className='text-2xl font-semibold text-'>Amadi Joseph</h3>
+          <button
+            type='button'
+            onClick={() =>
+              router.push('/v/dashboard/vote?position=Vice President')
+            }
+            className='px-7 py-3 text-white bg-primary rounded-md shadow-sm'
+          >
+            Vote
+          </button>
         </div>
-        <p className='text-xs font-medium'>03-04-2023</p>
       </div>
     </div>
   )
--- app/v/dashboard/page.tsx
+++ app/v/dashboard/page.tsx
@@ -5,54 +5,49 @@ import { useRouter } from 'next/navigation'
 import { useAuth } from '../auth/auth'
 
 export default function Timeline() {
-  const {voter} = useAuth()
+  const { voter } = useAuth()
 
   return (
-      <div className='flex flex-col min-h-screen gap-0'>
-        <div className='py-16'>
-          <h1 className='text-6xl font-bold'>Hello, {voter?.displayName}</h1>
+    <div className='flex flex-col min-h-screen gap-0'>
+      <div className='flex flex-col items-start py-10 gap-5'>
+        <h1 className='text-sm font-semibold'>Hello, {voter?.displayName}</h1>
+        <h1 className='text-6xl font-semibold'>Timeline</h1>
+      </div>
+      <div className='grow flex flex-col h-full'>
+        <div className='flex justify-start items-center gap-20'>
+          <Link href='/v/dashboard/'>
+            <p className='text-base font-semibold'>Timeline</p>
+          </Link>
+          <Link href='/v/dashboard/election'>
+            <p className='text-base font-semibold'>Election</p>
+          </Link>
         </div>
-        <div className='grow flex flex-col h-full'>
-          <div className='flex justify-start items-center gap-20'>
-            <Link href='/v/dashboard/'>
-              <p className='text-base font-semibold'>Timeline</p>
-            </Link>
-            <Link href='/v/dashboard/election'>
-              <p className='text-base font-semibold'>Election</p>
-            </Link>
+        <div className='grow h-full'>
+          <div className='flex flex-col gap-0'>
+            <VotedPositionAccordion />
+            <VotedPositionAccordion />
+            <VotedPositionAccordion />
+            <VotedPositionAccordion />
           </div>
-          <div className='grow h-full'>
-    <div className='flex flex-col gap-0'>
-      <PositionAccordion />
-      <PositionAccordion />
-      <PositionAccordion />
-      <PositionAccordion />
-    </div>
-    </div>
         </div>
       </div>
+    </div>
   )
 }
 
-function PositionAccordion() {
+function VotedPositionAccordion() {
   const router = useRouter()
 
   return (
-    <div className='bg-primary/30 first:border-t first:border-t-primary border-b border-b-primary px-14 py-7'>
+    <div className='bg-primary/10 first:border-t first:border-t-primary border-b border-b-primary px-14 py-7'>
       <div className='flex flex-col items-start gap-2'>
         <p className='text-xs font-medium'>QER313EHNT_HHGRI58Y3</p>
-        <div className='flex justify-between items-center gap-20'>
+        <div className='flex justify-start items-center gap-4'>
           <h3 className='text-4xl font-semibold text-'>Vice President</h3>
-          <button
-            type='button'
-            onClick={() =>
-              router.push('/v/dashboard/vote?position=Vice President')
-            }
-            className='px-7 py-3 text-white bg-primary rounded-md shadow-sm'
-          >
-            Vote
-          </button>
+          <h3 className='text-2xl font-semibold text-'>&rarr;</h3>
+          <h3 className='text-2xl font-semibold text-'>Amadi Joseph</h3>
         </div>
+        <p className='text-xs font-medium'>03-04-2023</p>
       </div>
     </div>
   )
