e5ebb18862a2e8cb9b924499cf04efc0f03a8484
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-18 08:42:55 +0100

    Fix: Format with prettier

--- lib/auth/getVoter.ts
+++ lib/auth/getVoter.ts
@@ -5,11 +5,11 @@ import { doc, getFirestore, getDoc } from 'firebase/firestore'
 const auth = getAuth(firebase_app)
 const db = getFirestore(firebase_app)
 
-export default async function getVoter(uid=auth.currentUser?.uid) {
+export default async function getVoter(uid = auth.currentUser?.uid) {
   if (uid) {
     const votersRef = doc(db, 'voters', uid)
     const docSnap = await getDoc(votersRef)
-  
+
     if (docSnap.exists()) return docSnap.data() as User
   }
 
