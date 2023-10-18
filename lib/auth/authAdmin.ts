075188be2f67574153974c61c34cf7bd786ecac5
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-18 13:15:57 +0100

    Fix: Refactor auth admin code

--- lib/auth/authAdmin.ts
+++ lib/auth/authAdmin.ts
@@ -1,12 +1,37 @@
 export default function authAdmin(rank: string, passcode: string) {
-  if (rank === 'PRESIDENT' && passcode === process.env.ADMIN_PRESIDENT_CODE) {
-    return {auth: process.env.ADMIN_PRESIDENT, id: process.env.ADMIN_PRESIDENT_SESSION}
-  } else if (rank === 'VICE PRESIDENT' && passcode === process.env.ADMIN_VICE_PRESIDENT_CODE) {
-    return {auth: process.env.ADMIN_VICE_PRESIDENT, id: process.env.ADMIN_VICE_PRESIDENT_SESSION}
-  } else if (rank === 'ELECTORIAL GOVERNOR' && passcode === process.env.ADMIN_ELECTORIAL_GOVERNOR_CODE) {
-    return {auth: process.env.ADMIN_ELECTORIAL_GOVERNOR, id: process.env.ADMIN_ELECTORIAL_GOVERNOR_SESSION}
-  } else if (rank === 'DEPUTY VICE GOVERNOR' && passcode === process.env.ADMIN_DEPUTY_VICE_PRESIDENT) {
-    return {auth: process.env.ADMIN_DEPUTY_VICE_PRESIDENT, id: process.env.ADMIN_DEPUTY_VICE_PRESIDENT_SESSION}
+  const isPresident =
+    rank === 'PRESIDENT' && passcode === process.env.ADMIN_PRESIDENT_CODE
+  const isVice =
+    rank === 'VICE PRESIDENT' &&
+    passcode === process.env.ADMIN_VICE_PRESIDENT_CODE
+  const isDeputyVice =
+    rank === 'DEPUTY VICE GOVERNOR' &&
+    passcode === process.env.ADMIN_DEPUTY_VICE_PRESIDENT
+  const isElect =
+    rank === 'ELECTORIAL GOVERNOR' &&
+    passcode === process.env.ADMIN_ELECTORIAL_GOVERNOR_CODE
+
+    
+  if (isPresident) {
+    return {
+      auth: process.env.ADMIN_PRESIDENT,
+      id: process.env.ADMIN_PRESIDENT_SESSION,
+    }
+  } else if (isVice) {
+    return {
+      auth: process.env.ADMIN_VICE_PRESIDENT,
+      id: process.env.ADMIN_VICE_PRESIDENT_SESSION,
+    }
+  } else if (isElect) {
+    return {
+      auth: process.env.ADMIN_ELECTORIAL_GOVERNOR,
+      id: process.env.ADMIN_ELECTORIAL_GOVERNOR_SESSION,
+    }
+  } else if (isDeputyVice) {
+    return {
+      auth: process.env.ADMIN_DEPUTY_VICE_PRESIDENT,
+      id: process.env.ADMIN_DEPUTY_VICE_PRESIDENT_SESSION,
+    }
   }
 
   return null
