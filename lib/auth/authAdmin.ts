df763c444067d03ea6a08423a5c7e7e987788b11
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-18 13:12:11 +0100

    Fix: Update the auth for admin

--- lib/auth/authAdmin.ts
+++ lib/auth/authAdmin.ts
@@ -1,16 +1,12 @@
 export default function authAdmin(rank: string, passcode: string) {
-  const authorized = {
-    [`${process.env.ADMIN_PRESIDENT}`]: process.env.ADMIN_PRESIDENT_CODE,
-    [`${process.env.ADMIN_VICE_PRESIDENT}`]: process.env.ADMIN_VICE_PRESIDENT_CODE,
-    [`${process.env.ADMIN_ELECTORIAL_GOVERNOR}`]: process.env.ADMIN_ELECTORIAL_GOVERNOR_CODE
-  }
-  
   if (rank === 'PRESIDENT' && passcode === process.env.ADMIN_PRESIDENT_CODE) {
-    return process.env.ADMIN_PRESIDENT
+    return {auth: process.env.ADMIN_PRESIDENT, id: process.env.ADMIN_PRESIDENT_SESSION}
   } else if (rank === 'VICE PRESIDENT' && passcode === process.env.ADMIN_VICE_PRESIDENT_CODE) {
-    return process.env.ADMIN_VICE_PRESIDENT
+    return {auth: process.env.ADMIN_VICE_PRESIDENT, id: process.env.ADMIN_VICE_PRESIDENT_SESSION}
   } else if (rank === 'ELECTORIAL GOVERNOR' && passcode === process.env.ADMIN_ELECTORIAL_GOVERNOR_CODE) {
-    return process.env.ADMIN_ELECTORIAL_GOVERNOR
+    return {auth: process.env.ADMIN_ELECTORIAL_GOVERNOR, id: process.env.ADMIN_ELECTORIAL_GOVERNOR_SESSION}
+  } else if (rank === 'DEPUTY VICE GOVERNOR' && passcode === process.env.ADMIN_DEPUTY_VICE_PRESIDENT) {
+    return {auth: process.env.ADMIN_DEPUTY_VICE_PRESIDENT, id: process.env.ADMIN_DEPUTY_VICE_PRESIDENT_SESSION}
   }
 
   return null
