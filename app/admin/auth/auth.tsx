dcda37cb87a13d75a88d8787b565812a714383e7
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-18 11:50:37 +0100

    Feat: Create the admin auth page and logic

---
+++ app/admin/auth/auth.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { createContext, useContext, useState, useEffect, useMemo } from 'react'
+import { useRouter } from 'next/navigation'
+import getVoter from '@/lib/auth/getVoter'
+
+type AuthContextType = {
+  admin: string | null
+  setAdmin?: (admin: string) => void
+}
+
+export const Auth = createContext<AuthContextType>({ admin: null })
+
+export const useAuth = () => useContext(Auth)
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [admin, setAdmin] = useState<string | null>(null)
+  const [loading, setLoading] = useState(true)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!admin) {
+      router.replace('/admin/auth/')
+    }
+    setLoading(false)
+  }, [admin])
+
+  return (
+    <Auth.Provider value={{ admin, setAdmin }}>
+      {loading ? (
+        <div className='flex justify-center items-center min-h-screen bg-black/30'>
+          Loading...
+        </div>
+      ) : (
+        children
+      )}
+    </Auth.Provider>
+  )
+}
---
+++ app/admin/auth/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import {useState, FormEvent} from 'react'
+
+export default function Auth() {
+  const [form ,setForm] = useState({
+    rank: '',
+    passcode: '',
+  })
+
+  const setRank = (rank: string) => setForm((prev) => ({ ...prev, rank }))
+  const setPasscode = (passcode: string) => setForm((prev) => ({ ...prev, passcode }))
+
+  const submit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // Submit the form
+  }
+
+  return (
+    <form onSubmit={submit}>
+      <div className='flex flex-col justify-center items-center gap-5 min-h-screen'>
+        <div className='flex flex-col justify-start items-start gap-2'>
+          <label htmlFor='passcode' className='text-base font-semibold'>
+            Passcode
+          </label>
+          <input
+            type='passcode'
+            id='passcode'
+            name='passcode'
+            placeholder='passcode'
+            className='border border-gray-400 rounded-md px-3 py-2'
+            onChange={(e) => setPasscode(e.target.value)}
+          />
+        </div>
+        <div className='flex flex-col justify-start items-center gap-5'>
+          <button
+            type='submit'
+            className='px-7 py-3 text-white bg-primary rounded-md shadow-sm'
+          >
+            Sign in
+          </button>
+        </div>
+      </div>
+    </form>
+  )
+}
