14e8f7e149ffd2fe8dad310ac86811d04ae4528f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-18 13:45:01 +0100

    Feat: Create the layout class

---
+++ app/admin/layout.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { AuthProvider } from './auth/auth'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <AuthProvider>{children}</AuthProvider>
+  )
+}
