0255f81f1fe5d55dc47babf874309e89b80154a7
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-19 14:33:42 +0100

    Feat: Adjust the create position fn

--- lib/candidate/create.ts
+++ lib/candidate/create.ts
@@ -5,9 +5,9 @@ import type { CandidateType } from '@/app/admin/dashboard/create/page'
 const db = getFirestore(firebase_app)
 const positionsRef = collection(db, 'candidates')
 
-export default async function create(candidate: CandidateType) {
+export default async function createCandidate(candidate: CandidateType) {
   let candidateData, error
-  
+
   try {
     candidateData = await addDoc(positionsRef, candidate)
   } catch (e) {
--- lib/position/create.ts
+++ lib/position/create.ts
@@ -1,16 +1,22 @@
 import firebase_app from '../firebase'
 import { getFirestore, addDoc, collection } from 'firebase/firestore'
 import type { CandidateType } from '@/app/admin/dashboard/create/page'
+import createCandidate from '../candidate/create'
 
 const db = getFirestore(firebase_app)
 const positionsRef = collection(db, 'positions')
 
-export default async function create(title: string, candidates: CandidateType[], creator: string) {
+export default async function createPosition(title: string, candidates: CandidateType[], creator: string) {
   let position, error
   try {
+    const candidatesId = candidates.map(async (candidate) => {
+      const {candidateData} = await createCandidate(candidate)
+      return candidateData?.id
+    })
+
     const data = {
       title,
-      candidates,
+      candidates: [...candidatesId],
       creator,
     }
 
