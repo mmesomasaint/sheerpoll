54beaaf0db99d7038a8dacd4d614646f992fbc38
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-20 13:58:09 +0100

    Fix: Design the get fn for candidates

---
+++ lib/candidate/get.ts
@@ -0,0 +1,31 @@
+import FirebaseApp from '@/lib/firebase'
+import {
+  doc,
+  getFirestore,
+  getDoc,
+  query,
+  where,
+  collection,
+  getDocs,
+} from 'firebase/firestore'
+
+const db = getFirestore(FirebaseApp)
+const candidatesRef = collection(db, 'candidates')
+
+export async function getById(uid: string) {
+  if (uid) {
+    const docSnap = await getDoc(doc(candidatesRef, uid))
+
+    if (docSnap.exists()) return docSnap.data()
+  }
+
+  return null
+}
+
+export async function getByPosition(positionId: string) {
+  const q = query(candidatesRef, where('position_id', '==', positionId))
+  const querySnap = await getDocs(q)
+  const docs = querySnap.docs.map((doc) => doc.data())
+
+  return docs
+}
