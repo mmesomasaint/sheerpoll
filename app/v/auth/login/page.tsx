ab28025ac09e9e4ddd95015e1eb82a5356d1a64a
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-21 10:23:57 +0100

    Feat: Make password input controlled

--- app/v/auth/login/page.tsx
+++ app/v/auth/login/page.tsx
@@ -54,6 +54,7 @@ export default function Login() {
             type='password'
             id='password'
             name='password'
+            value={form.password}
             placeholder='Password'
             className='border border-gray-400 rounded-md px-3 py-2 w-full'
             onChange={(e) => setPassword(e.target.value)}
