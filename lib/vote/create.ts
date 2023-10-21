16451b8d4b09bd4d43966b4301c0a158c24bda53
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-22 00:14:04 +0100

    Fix: Format with prrettier

--- lib/vote/create.ts
+++ lib/vote/create.ts
@@ -5,7 +5,7 @@ import {
   addDoc,
   setDoc,
   collection,
-  arrayUnion
+  arrayUnion,
 } from 'firebase/firestore'
 
 const db = getFirestore(FirebaseApp)
@@ -14,7 +14,7 @@ const votesRef = collection(db, 'votes')
 export default async function createVote(
   positionId: string,
   candidateId: string,
-  voterId: string,
+  voterId: string
 ) {
   let vote, error
 
@@ -22,19 +22,27 @@ export default async function createVote(
     const voteData = {
       position_id: positionId,
       candidate_id: candidateId,
-      voter_id: voterId
+      voter_id: voterId,
     }
     const voteDoc = await addDoc(votesRef, voteData)
 
     // Add the vote id to the votes of position, candidate and voter.
     const positionRef = doc(db, 'positions', positionId)
-    await setDoc(positionRef, {votes: arrayUnion(voteDoc.id)}, {merge: true})
+    await setDoc(
+      positionRef,
+      { votes: arrayUnion(voteDoc.id) },
+      { merge: true }
+    )
 
     const candidateRef = doc(db, 'candidates', candidateId)
-    await setDoc(candidateRef, {votes: arrayUnion(voteDoc.id)}, {merge: true})
+    await setDoc(
+      candidateRef,
+      { votes: arrayUnion(voteDoc.id) },
+      { merge: true }
+    )
 
     const voterRef = doc(db, 'voters', voterId)
-    await setDoc(voterRef, {votes: arrayUnion(voteDoc.id)}, {merge: true})
+    await setDoc(voterRef, { votes: arrayUnion(voteDoc.id) }, { merge: true })
 
     vote = voteDoc.id
   } catch (e) {
