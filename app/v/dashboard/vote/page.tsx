0e0937189c04149009732460ae7482e7f3c88abc
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-21 23:12:30 +0100

    Fix: Format with prrettier

--- app/v/dashboard/vote/page.tsx
+++ app/v/dashboard/vote/page.tsx
@@ -14,10 +14,14 @@ export default function Timeline() {
   const [loading, setLoading] = useState(true)
   const [position, setPosition] = useState<DocumentData | null>()
 
-  const totalVotes = useMemo(() => position?.candidates.reduce(
-    (acc: number, cur: DocumentData) => acc + cur.votes.length,
-    0
-  ), [position])
+  const totalVotes = useMemo(
+    () =>
+      position?.candidates.reduce(
+        (acc: number, cur: DocumentData) => acc + cur.votes.length,
+        0
+      ),
+    [position]
+  )
 
   useEffect(() => {
     setLoading(true)
@@ -32,33 +36,36 @@ export default function Timeline() {
 
   return (
     <div className='flex flex-col min-h-screen gap-0 px-24'>
-    <h1 className='text-sm font-medium pt-5'>Vote A Candidate</h1>
+      <h1 className='text-sm font-medium pt-5'>Vote A Candidate</h1>
       <div className='flex justify-between items-center py-5 gap-5'>
-          <div className='flex flex-col items-start gap-0'>
-            <p className='text-sm font-semibold text-black/60'>TITLE</p>
-        <p className='text-4xl font-bold uppercase'>Vice President</p>
-          </div>
-          <div className='flex flex-col items-end gap-0'>
-            <p className='text-sm font-semibold text-black/60'>TOTAL VOTES</p>
-            <p className='text-xl text-center font-semibold uppercase w-full py-1 border border-primary text-primary rounded-md'>534</p>
-          </div>
+        <div className='flex flex-col items-start gap-0'>
+          <p className='text-sm font-semibold text-black/60'>TITLE</p>
+          <p className='text-4xl font-bold uppercase'>Vice President</p>
         </div>
+        <div className='flex flex-col items-end gap-0'>
+          <p className='text-sm font-semibold text-black/60'>TOTAL VOTES</p>
+          <p className='text-xl text-center font-semibold uppercase w-full py-1 border border-primary text-primary rounded-md'>
+            534
+          </p>
+        </div>
+      </div>
       <div className='grow flex flex-col w-full'>
         <div className='flex justify-center items-center gap-10 pb-1 border-b border-b-primary/60'>
           <div className='relative'>
             <div className='px-7 flex justify-center items-center gap-3 w-fit'>
-              <BsStar
-                className='text-primary text-base'
-              />
+              <BsStar className='text-primary text-base' />
               <p className='text-base font-semibold'>Candidates</p>
             </div>
-            <div
-              className='absolute -bottom-[0.415rem] w-full border-2 border-primary'
-            />
+            <div className='absolute -bottom-[0.415rem] w-full border-2 border-primary' />
           </div>
         </div>
-    <div className='grow flex px-20 flex-col justify-start w-full'>
-    <CandidateCard name={'Obi'} votes={45} active={false} onVoteClick={() => setChoice('abc')} />
+        <div className='grow flex px-20 flex-col justify-start w-full'>
+          <CandidateCard
+            name={'Obi'}
+            votes={45}
+            active={false}
+            onVoteClick={() => setChoice('abc')}
+          />
           <button
             type='submit'
             disabled={!choice}
@@ -66,18 +73,17 @@ export default function Timeline() {
           >
             Submit
           </button>
-    </div>
+        </div>
       </div>
     </div>
   )
 }
 
-
 function CandidateCard({
   name,
   votes,
   active,
-  onVoteClick
+  onVoteClick,
 }: {
   name: string
   votes: number
@@ -86,22 +92,27 @@ function CandidateCard({
 }) {
   return (
     <div className='border-b border-b-primary/60 px-20 py-10 shadow-sm w-full'>
-    <div className='grid grid-cols-3 gap-10 place-items-center'>
-      <div className='flex flex-col items-center gap-0'>
-        <p className='text-sm font-semibold text-black/60'>NAME</p>
-        <p className='text-xl font-semibold uppercase'>NNAMDI KANU</p>
-      </div>
-      <div className='flex flex-col items-center gap-0'>
-        <p className='text-sm font-semibold text-black/60'>VOTES</p>
-        <p className='text-xl font-semibold uppercase'>343</p>
-      </div>
-      <div className='flex flex-col items-center gap-0'>
-        <p className='text-sm font-semibold text-black/60'>VOTE</p>
-        <div className={`w-10 h-10 rounded-sm border-4 ${active ? 'border-primary' : 'border-black/60'} flex justify-center items-center shadow-sm`} onClick={onVoteClick}>
-          {active && <BsStar className='text-primary text-base' />}
+      <div className='grid grid-cols-3 gap-10 place-items-center'>
+        <div className='flex flex-col items-center gap-0'>
+          <p className='text-sm font-semibold text-black/60'>NAME</p>
+          <p className='text-xl font-semibold uppercase'>NNAMDI KANU</p>
+        </div>
+        <div className='flex flex-col items-center gap-0'>
+          <p className='text-sm font-semibold text-black/60'>VOTES</p>
+          <p className='text-xl font-semibold uppercase'>343</p>
+        </div>
+        <div className='flex flex-col items-center gap-0'>
+          <p className='text-sm font-semibold text-black/60'>VOTE</p>
+          <div
+            className={`w-10 h-10 rounded-sm border-4 ${
+              active ? 'border-primary' : 'border-black/60'
+            } flex justify-center items-center shadow-sm`}
+            onClick={onVoteClick}
+          >
+            {active && <BsStar className='text-primary text-base' />}
+          </div>
         </div>
       </div>
-    </div>
     </div>
   )
 }
