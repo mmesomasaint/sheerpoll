9e47bacaf10caac86e34a90571630e965958e601
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-22 19:52:16 +0100

    Feat: Create the getError fxn

---
+++ lib/getError.ts
@@ -0,0 +1,31 @@
+import { AuthError } from "firebase/auth";
+
+export default function getError(error: AuthError) {
+  if (error.code === 'auth/email-already-in-use') {
+    return {
+      from: ['email'],
+      message: 'Email already in use'
+    }
+  } else if (error.code === 'auth/invalid-email') {
+    return {
+      from: ['email'],
+      message: "Email doesn't exist"
+    }
+  } else if (error.code === 'auth/invalid-password') {
+    return {
+      from: ['password'],
+      message: "Password doesn't exist"
+    }
+  } else if (error.code === 'auth/user-not-found') {
+    return {
+      from: ['email', 'password'],
+      message: "Voter doesn't exist"
+    }
+  }
+
+  
+  return {
+    from: [''],
+    message: 'Something went wrong'
+  }
+}
