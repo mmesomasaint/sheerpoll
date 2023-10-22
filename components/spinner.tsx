bb1a0f483d97528eb8fb7f0e70639244e8dc51a0
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-22 15:06:22 +0100

    Fix: Format with prrettier

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
-    <div className="flex justify-center items-center">
-      <div className="w-8 h-8 border-b-2 border-gray-900 rounded-full animate-spin" />
+    <div className='flex justify-center items-center'>
+      <div className='w-8 h-8 border-b-2 border-gray-900 rounded-full animate-spin' />
     </div>
-  );
+  )
 }
